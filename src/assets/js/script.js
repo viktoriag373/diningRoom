72488eeef5ce95c874d2a8ea0e46e532188c3209
Vika <viktoriag373@gmail.com>
2021-03-24 01:22:04 +0300

    1. Сделан поиск 2. Сделан выбор категорий  для поиска 3. Сделана корзина

---
+++ src/assets/js/script.js
@@ -0,0 +1,34 @@
+'use strict'
+
+window.onload = function () {
+
+    //получаем идентификатор элемента
+	const searchCategoriesFilter = document.querySelector(".search-categories__filter");
+	const searchCategoriesSubnav = document.querySelector(".search-categories__subnav");
+	const searchCategoriesCategoryItem = document.querySelectorAll(".search-categories__category-item");
+	const searchCategoriesFilterValue = document.querySelector(".search-categories__filter-value");
+
+	function categoriesSubnavToggle() {
+		searchCategoriesSubnav.classList.toggle("search-categories__subnav--visible");
+	}
+
+	function categoriesSubnavClose() {
+		searchCategoriesSubnav.classList.remove("search-categories__subnav--visible");
+	}
+
+	function onCategoryClick(event) {
+		searchCategoriesFilterValue.innerText = event.target.innerText;
+		categoriesSubnavClose();
+	}
+	
+
+	searchCategoriesFilter.onclick = categoriesSubnavToggle;
+
+	for (let index = 0; index < searchCategoriesCategoryItem.length; index++) {
+		searchCategoriesCategoryItem[index].onclick = onCategoryClick;
+	}
+}
+
+
+
+
