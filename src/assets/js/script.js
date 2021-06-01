76068fe35a3efb379088aabdcc5e07d20e0ad923
Vika <viktoriag373@gmail.com>
2021-06-01 19:45:55 +0300

    Разработка адаптива + мелкие правки

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -7,6 +7,9 @@ window.onload = function () {
 	const searchCategoriesSubnav = document.querySelector(".search-categories__subnav");
 	const searchCategoriesCategoryItem = document.querySelectorAll(".search-categories__category-item");
 	const searchCategoriesFilterValue = document.querySelector(".search-categories__filter-value");
+	const modalContactUsClose = document.querySelector(".modal-contact-us__close");
+	const modalContactUs = document.querySelector(".modal-contact-us");
+	const menuItemTel = document.querySelector(".menu__item-tel .menu__link");
 
 	function categoriesSubnavToggle() {
 		searchCategoriesSubnav.classList.toggle("search-categories__subnav--visible");
@@ -21,7 +24,19 @@ window.onload = function () {
 		categoriesSubnavClose();
 	}
 	
+	function modalFormClose() {
+		modalContactUs.classList.remove("modal-contact-us--visible");
+		modalContactUs.classList.add("modal-contact-us--hidden");
+	}
 
+	function modalFormOpen() {
+		modalContactUs.classList.remove("modal-contact-us--hidden");
+		modalContactUs.classList.add("modal-contact-us--visible");
+	}
+
+
+	menuItemTel.onclick = modalFormOpen;
+	modalContactUsClose.onclick = modalFormClose;
 	searchCategoriesFilter.onclick = categoriesSubnavToggle;
 
 	for (let index = 0; index < searchCategoriesCategoryItem.length; index++) {
